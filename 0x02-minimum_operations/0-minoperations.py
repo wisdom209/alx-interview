d4d1d03f89d75346dd2278fec41abb5cea48ced3
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-13 21:17:35 +0100

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""module for minimum copy pase operations"""
+
+
+def minOperations(n):
+    """function for minimum copy paste operations"""
+    num_chars_in_file = 1
+    num_chars_in_clipboard = 0
+    num_operations = 0
+
+    while num_chars_in_file < n:
+        if num_chars_in_clipboard == 0:
+            num_chars_in_clipboard = num_chars_in_file
+            num_operations += 1
+
+        if num_chars_in_file == 1:
+            num_chars_in_file += num_chars_in_clipboard
+            num_operations += 1
+            continue
+
+        remaining_chars_to_paste = n - num_chars_in_file
+
+        if remaining_chars_to_paste < num_chars_in_clipboard:
+            return 0
+
+        if remaining_chars_to_paste % num_chars_in_file != 0:
+            num_chars_in_file += num_chars_in_clipboard
+            num_operations += 1
+        else:
+            num_chars_in_clipboard = num_chars_in_file
+            num_chars_in_file += num_chars_in_clipboard
+            num_operations += 2
+
+    if num_chars_in_file == n:
+        return num_operations
+    else:
+        return 0
