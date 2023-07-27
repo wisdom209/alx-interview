5affc0e09bbded4bb0e459ed3b782272471bbe42
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-27 14:09:43 +0100

    Utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,33 +12,30 @@ def get_significant_bits(num):
     return set_bits
 
 
-def check_next_valid_bytes(num_set_bit, char_list, curr_index):
-    """check next valid bytes"""
-    num_bytes_to_check = num_set_bit - 1
-    if (curr_index + num_bytes_to_check) < len(char_list):
-        count = 1
-        for _ in range(num_bytes_to_check):
-            byte_to_check = char_list[curr_index + count]
-            count += 1
-            if (byte_to_check & 1 << 7) and not (byte_to_check & 1 << 6):
-                continue
-            else:
-                return -1
-    else:
-        return -1
-    return curr_index + num_set_bit
+def check_continuation_bytes(num_set_bits, index, data):
+    """
+    Check if the continuation bytes of a multi-byte UTF-8 sequence are valid,
+    given the number of set bits.
+    """
+    for i in range(index + 1, index + num_set_bits):
+        if i >= len(data):
+            return False
+        if (data[i] & 0b11000000) != 0b10000000:
+            return False
+    return True
 
 
 def validUTF8(data):
     """check valid utf8"""
-    for index in range(len(data)):
-        set_bits = get_significant_bits(data[index])
-        if set_bits == 1 or set_bits > 4:
+    i = 0
+    while i < len(data):
+        num_set_bits = get_significant_bits(data[i])
+        if num_set_bits == 1 or num_set_bits > 4:
             return False
-        if set_bits == 0:
-            continue
-        next_index = check_next_valid_bytes(set_bits, data, index)
-        if next_index == -1:
+        if not check_continuation_bytes(num_set_bits, i, data):
             return False
-        index = next_index
+        if num_set_bits == 0:
+            i += 1
+        else:
+            i += num_set_bits
     return True
