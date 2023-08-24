7e74045b32b0f272d9c60bc1033019f703dd3936
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-24 16:48:32 +0100

    Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that determine the fewest number of
+coins needed to meet a given amount total"""
+
+
+def makeChange(coins, total):
+    """function that performs action of module"""
+    total_count = 0
+    coins.sort(reverse=True)
+
+    if total <= 0:
+        return 0
+    for x in coins:
+        counter = total // x
+        total = total % x
+        total_count += counter
+        if total == 0:
+            return total_count
+    return -1
