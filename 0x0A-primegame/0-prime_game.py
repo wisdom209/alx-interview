30fb198922773d16ae4e9aa5ea0d36f1996a6487
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-09-08 04:04:40 +0100

    update

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Prime game module"""
+
+
+def isPrime(num):
+    """determine if a number is prime"""
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    for x in range(2, num // 2):
+        if num % x == 0:
+            return False
+    return True
+
+
+def removeMultiples(num, arr):
+    """remove multiples of num in arr"""
+    new_arr = []
+    for x in arr:
+        if x % num != 0:
+            new_arr.append(x)
+    return new_arr
+
+
+def find_prime_in_set(arr):
+    """find a prime number in set"""
+    for num in arr:
+        if isPrime(num):
+            return num
+    return None
+
+
+def isWinner(numRounds, rounds):
+    """Prime game function"""
+    maria_wins = 0
+    ben_wins = 0
+    countRounds = 0
+
+    for round in rounds:
+        if countRounds == numRounds:
+            break
+
+        countRounds += 1
+        count = 0
+
+        round_set = [x for x in range(1, round + 1)]
+
+        while True:
+            primeNum = find_prime_in_set(round_set)
+            if (primeNum is None):
+                break
+            round_set = removeMultiples(primeNum, round_set)
+            if round_set is None or round_set == []:
+                break
+            count += 1
+        if count != 0 and count % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    print(ben_wins, maria_wins)
+    if ben_wins > maria_wins:
+        return "Ben"
+    elif maria_wins > ben_wins:
+        return "Maria"
+
+    return None
---
+++ 0x0A-primegame/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+isWinner = __import__('0-prime_game').isWinner
+
+print("Winner: {}".format(isWinner(5, [4, 5, 1, 5, 5])))
